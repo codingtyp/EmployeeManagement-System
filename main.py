6750d87eaac3e40c3a5d9f3f6a3c160a8a13f062
Klyde <klydefb@gmail.com>
2022-05-30 22:45:42 +0800

    Major update

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
+class Employee:
+    empLast = ""
+    empFirst = ""
+    empMiddle = ""
+    empAge = 0
+    empAddress = ""
+    empBirth = ""
+
+
+    def autoData(self, empLast, empFirst, empMiddle, empAge, empAddress, empBirth):
+        self.empLast = empLast
+        self.empFirst = empFirst
+        self.empMiddle = empMiddle
+        self.empAge = empAge
+        self.empAddress = empAddress
+        self.empBirth = empBirth
+
+    def setData(self):
+        self.empLast = input("Last name:   ")
+        self.empFirst = input("First name:   ")
+        self.empMiddle = input("Last name:   ")
+        self.empAge = int(input("Age:   "))
+        self.empAddress = input("Address:   ")
+        self.empBirth = input("Date of Birth:   ")
+
+
+    def displayData(self):
+        print("Name:           ", self.empLast, ",",self.empFirst, self.empMiddle)
+        print("Age:            ", self.empAge)
+        print("Address:        ", self.empAddress) 
+        print("Date of Birth:  ", self.empBirth)
+
+
+        
+
+class Work(Employee):
+    def __init__(self, empPosition, empDepartment, empSalary):
+        self.empPosition = empPosition
+        self.empDepartment = empDepartment
+        self.empSalary = empSalary
+
+
+
+
 def main_menu():
     print("*********************************************************")
-    print("*             EMPLOYEE MANAGEMENT SYSTEM                  *")
+    print("*              EMPLOYEE MANAGEMENT SYSTEM               *")
     print("*********************************************************")
     print("")
 
@@ -16,15 +60,15 @@ def main_menu():
             menuChoice = input("Choice: ")
 
 
-            if menuChoice.lower == 'c':
-                ()
-            elif menuChoice.lower == 'r':
-                ()
-            elif menuChoice.lower == 'u':
-                ()
-            elif menuChoice.lower == 'd':
-                ()
-            elif menuChoice.lower == 'e':
+            if menuChoice == 'c':
+                create_data()
+            elif menuChoice == 'r':
+                read_data()
+            elif menuChoice == 'u':
+                update_data()
+            elif menuChoice == 'd':
+                delete_data()
+            elif menuChoice == 'e':
                 exit_program()
             else:
                 print("\n")
@@ -44,15 +88,27 @@ def create_data():
     print("")
 
 
+    emp = Employee()
 
+    generateChoice = input("Do you want to auto-generate data?   ")
+
+    if generateChoice == 'y':
+        emp.setData()
+    elif generateChoice == 'n':
+        emp.autoData("Doe", "John", "Smith", 20, "Recodo", "September")
+
+    emp.displayData()
+
+
+    
 
 
 # FUNCTION: READ EMPLOYEE DATA
 def read_data():
     print("")
 
-
-
+    emp = Employee()
+    emp.displayData()
 
 
 # FUNCTION: UPDATE EMPLOYEE DATA
