f7ece28f4dc08607a1624a9047a539c0d9054f85
Klyde <klydefb@gmail.com>
2022-06-01 22:00:09 +0800

    Update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Employee Management Sytem
 
+<br/>
+<br/>
+
 ## :page_with_curl: FEATURES:
 - Create data
   - input *x* data
@@ -10,8 +13,8 @@
 <br/>
 
 ![GitHub](https://img.shields.io/github/license/codingtyp/EmployeeManagement-System?label=License&style=flat-square)
-![GitHub top language](https://img.shields.io/github/languages/top/codingtyp/EmployeeCRUD-System?label=Python&style=flat-square)
-![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/codingtyp/EmployeeCRUD-System?label=Code%20Size&style=flat-square)
+![GitHub top language](https://img.shields.io/github/languages/top/codingtyp/EmployeeManagement-System?label=Python&style=flat-square)
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/codingtyp/EmployeeManagement-System?label=Code%20Size&style=flat-square)
 
 ![GitHub last commit](https://img.shields.io/github/last-commit/codingtyp/EmployeeManagement-System?label=Last%20Commit&style=flat-square)
 ![GitHub commit activity](https://img.shields.io/github/commit-activity/m/codingtyp/EmployeeManagement-System?label=Commit%20Activity&style=flat-square)
--- main.py
+++ main.py
@@ -17,22 +17,16 @@ def main_menu():
         try:
             print("[C]reate")
             print("[R]ead")
-            print("[U]pdate")
-            print("[D]elete")
             print("[E]xit")
             print("---------")
             menuChoice = input("Choice: ")
 
 
-            if menuChoice == 'c':
+            if menuChoice == 'C' or menuChoice == 'c':
                 create_data()                        
-            elif menuChoice == 'r':    
+            elif menuChoice == 'R' or menuChoice == 'r':    
                 read_data(employee_list)
-            elif menuChoice == 'u':
-                pass 
-            elif menuChoice == 'd':
-                pass
-            elif menuChoice == 'e':
+            elif menuChoice == 'E' or menuChoice == 'e':
                 exit_program()
             else:
                 print("\n")
@@ -44,41 +38,84 @@ def main_menu():
             print("\n")
 
 
+
+
+
 # FUNCTION TO TAKE USER INPUT, STORE IN THE OBJECT, THEN STORE IN A LIST
-def create_data():
-    # Input x employees
-    totalEmployees = int(input("How many employees? "))
+def create_data():    
+    print()
+
+
+    # Determine how many data will be entered
+    while True:
+        try:
+            totalEmployees = int(input("How many employees?   "))
+            print()
+            break
+        except ValueError:
+            print("\n")
+            print(">> INVALID INPUT!")
+            print("\n")
 
     # Loop to iterate through each employee
     for i in range(totalEmployees): 
         # Input   
-        print("\n--------------------")
-        print('Enter the data for employee', int(i + 1))   
+        print("-----------", int(i + 1), "-----------") 
         empID = int(input("ID?   "))
         empLast = input("Last name?   ")
         empFirst = input("First name?   ")
         empMiddle = input("Middle name?   ")
-        print("--------------------\n")
-        print()
+        print("-------------------------")
+        print("\n")
+    
 
-
-        
         worker = Employee(empID, empLast, empFirst, empMiddle)                  # Creates the object to store the atributes in
         employee_list.append(worker)                                            # append the data into a list
     return employee_list
 
 
-def read_data(employee_list):
-    for employee in employee_list:
-        print("ID:               ", employee.get_empID())
-        print("Last name:        ", employee.get_empLast())
-        print("First name:       ", employee.get_empFirst())
-        print("Middle name:      ", employee.get_empMiddle(),sep='')
+
+
+
+# FUNCTION TO SIMPLY DISPLAY ALL DATA
+def read_data(employee_list):  
+    print("\n")
+
+
+    viewData = input("How do you like to view the data?  [l-ist/t-able]   ")
+
+    if viewData == 'L' or viewData == 'l':
         print()
+        print ("{:<8} {:<15} {:<10}".format('Name','Age','Percent'))
+        for employee in employee_list:
+            print("+--------------+----------------")
+            print("| ID           | ", employee.get_empID())
+            print("| Last name    | ", employee.get_empLast())
+            print("| First name   | ", employee.get_empFirst())
+            print("| Middle name  | ", employee.get_empMiddle())
+            print("+--------------+----------------")
+            print("\n")
+    elif viewData == 'T' or viewData == 't':
+        print()
+        print("+=======+==============+================+==================+")
+        print ("{:<5} {:<10} {:<10} {:<15} ".
+        format('|   ID  | ',' Last name  | ',' First name   | ', '  Middle name   |'))
+        print("+=======+==============+================+==================+")
+
+        for employee in employee_list:
+            print ("{:<5} {:<10} {:<10} {:<15}".format( employee.get_empID(), employee.get_empLast(), employee.get_empFirst(), employee.get_empMiddle()))
+    else:
+        print("\n")
+        print(">> INVALID INPUT!")
+        print("\n")
+
+
+
+
 
 # FUNCTION TO EXIT THE PROGRAN
 def exit_program():
-    print("THANK YOU FOR USING THE SYSTEM!")
+    print("THANK YOU VERY MUCH!")
     exit()
 
 
